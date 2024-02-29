5df93f95a35a38d6caa5913ae3fd18e4000d8def
macmogu <mm@moguf2k.com>
2024-03-01 00:30:46 +0800

    update data

---
+++ ecf.org.cn/fatchdetailpage.py
@@ -0,0 +1,30 @@
+import requests
+
+headers = {
+    'authority': 'user.ecf.org.cn',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'cache-control': 'no-cache',
+    'pragma': 'no-cache',
+    'referer': 'https://user.ecf.org.cn/product/ss.aspx',
+    'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+}
+
+params = {
+    'id': '4496',
+}
+
+response = requests.get('https://user.ecf.org.cn/product/s.aspx', params=params, headers=headers)
+
+print(response.text)
+print(response.status_code)
+with open('./data/detail_4496.html', 'w', encoding='utf-8') as f:
+    f.write(response.text)
